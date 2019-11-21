7bb3d1c7dd3d2fe46313fdf7f74545fc126ac161
Joao Pedro de Munno Durante <joao.durante@venturus.org.br>
2019-11-21 10:52:55 -0300

    Rds removed

--- rds/common/createTable.js
+++
@@ -1,9 +0,0 @@
-module.exports.createTable = async (client) => {
-    const res = await client.query(`
-        CREATE TABLE IF NOT EXISTS test (
-            id SERIAL NOT NULL PRIMARY KEY,
-            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            message VARCHAR(100) NOT NULL
-        )
-    `)
-}
